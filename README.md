21bf50ff7c6c86d20c4cdd0d99d2a9955b7a074e
Sky Zhang <zhangsky999@gmail.com>
2025-05-11 18:28:16 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+PROJECT NOTES:
+
+5/5/2025
+- Instantiated new kernel and env "dna-seq-analyzer"
+- Imported a bacterial genome seq object from NCBI
+- Generated dict from the seq using package SeqIO
+- NOTES: Continue with analysis and handling of data. Learn from chatGPT. Using visualization next!
+
+5/6/2025
+- Practiced base_counts(seq) using a dict comprehension
+- Transposed counts.items() into bases, freqs via zip(*…) (PRACTICE!!!)
+- Matplotlib workflow
+- Reserved a canvas with plt.figure(figsize=(…, …))
+- Drew bars with plt.bar(x, height, color=…, width=…)
+- Labeled axes and title, rendered with plt.show()
+- Customized bar colors and widths
+
+
+
+
+
---
+++ src/sequence_utils.py
@@ -0,0 +1,33 @@
+from typing import List, Tuple, Union
+from Bio.Seq import Seq
+
+SeqType = Union[str, Seq] #A custom type which for readability encapsulates the str and Seq types
+
+def base_counts(seq: SeqType) -> dict[str, int]:
+    return {nt: seq.count(nt) for nt in "ACGT"}
+
+def gc_contents(seq: SeqType) -> float:
+
+    gc = 0
+    cc = 0
+
+    for base in seq:
+        if base == "G":
+            gc+=1
+        elif base == "C":
+            cc+=1
+
+    return 100 * ((gc+cc)/len(seq))
+
+def sliding_gc(seq: SeqType, window_size: int, step: int) -> Tuple[List[int], List[float]]:
+
+    positions = []
+    gc_values = []
+
+    for i in range(0, len(seq)-window_size+1,step):
+        positions.append(i)
+        gc_values.append(gc_contents(seq[i:i+window_size]))
+        
+    return positions, gc_values
+
+
